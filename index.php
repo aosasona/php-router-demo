424c0bc3d6eadafe2d82179b7429b2a8e65f0d93
Ayodeji O <osasonaayodeji1@gmail.com>
2022-08-20 19:34:40 +0100

    Updated to render instead of use

--- index.php
+++ index.php
@@ -1,25 +1,27 @@
 <?php
 require('vendor/autoload.php');
 
-use Trulyao\PhpRouter;
+use Trulyao\PhpRouter\Router as Router;
+use Trulyao\PhpRouter\HTTP\Response as Response;
+use Trulyao\PhpRouter\HTTP\Request as Request;
 
-$router = new PhpRouter\Router(__DIR__ . "/src", "");
+$router = new Router(__DIR__ . "/src", "");
 
-$router->get("/", function ($req, $res) {
-    return $res->use("views/welcome.php");
+$router->get("/", function (Request $req, Response $res) {
+    return $res->render("views/welcome.php");
 });
 
-$router->get("/about", function ($req, $res) {
-    return $res->use("views/about.php");
+$router->get("/about", function (Request $req, Response $res) {
+    return $res->render("views/about.php");
 });
 
 
-$router->get("/handler", function ($req, $res) {
-    include("src/controllers/post.php");
+$router->get("/handler", function (Request $req, Response $res) {
+    include("src/controllers/post.php"); // this is a controller file
     return hello($req, $res);
 });
 
-$router->get("/:name", function ($req, $res) {
+$router->get("/:name", function (Request $req, Response $res) {
     return $res->send("<h1 style='font-family: Arial; text-align: center; font-size: 5rem; margin: 10vh 0;'>Hello <span style='color: #39e; text-decoration: underline;'>{$req->params("name")}</span></h1>")->status(200);
 });
 
